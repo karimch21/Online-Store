b95e5cee9a37f3b74fb088db37c64485a84fdb20
DzmitrySm <magicjoker@yandex.ru>
2022-12-17 23:19:19 +0300

    feat: add basic configuration of the project

---
+++ webpack.dev.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000
+    },
+};
